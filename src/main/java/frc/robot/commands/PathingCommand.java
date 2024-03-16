93e477b77f458997644fae78c07bb4261a46c627
TonyBaston <tony.baston@gmail.com>
2024-03-16 14:42:21 -0500

    Slows down to take cuves slowly

--- src/main/java/frc/robot/commands/PathingCommand.java
+++ src/main/java/frc/robot/commands/PathingCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.Trajectory.State;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
@@ -31,32 +32,38 @@ public class PathingCommand extends Command{
     }
     boolean done=false;
     public void execute(){
-        Trajectory myPath=null;
+        Trajectory path=null;
         try {
-            myPath = pathfinder.generateTrajectory(drive.getPose(), new Pose2d(8, 4, new Rotation2d()), config);
+            path = pathfinder.generateTrajectory(drive.getPose(), new Pose2d(8, 4, new Rotation2d()), config);
         } catch (ImpossiblePathException e) {
-            System.out.println("No Path");
             drive.drive(0, 0, 0, true, false);
             velocity=0;
             SmartDashboard.putNumber("Velocity",0);
             done=true;
             return;
         }
-          State state=myPath.getStates().get(0);
-          State nextState=myPath.getStates().get(1);
-          double dX=nextState.poseMeters.getX()-state.poseMeters.getX();
-          double dY=nextState.poseMeters.getY()-state.poseMeters.getY();
+          State thisState=path.getStates().get(0);
+          State nextState=path.getStates().get(1);
+          double dX=nextState.poseMeters.getX()-thisState.poseMeters.getX();
+          double dY=nextState.poseMeters.getY()-thisState.poseMeters.getY();
           double total=Math.abs(dX)+Math.abs(dY);
-          double totalDistance=myPath.getTotalTimeSeconds()-(myPath.getStates().get(myPath.getStates().size()-1).timeSeconds-myPath.getStates().get(myPath.getStates().size()-2).timeSeconds);
-          velocity=profile.calculate(.02, new TrapezoidProfile.State(0,velocity), new TrapezoidProfile.State(totalDistance,0)).velocity;
-          
-          SmartDashboard.putNumber("Distance Left",totalDistance);
-          SmartDashboard.putNumber("Velocity",velocity);
+          velocity=profile.calculate(.02, new TrapezoidProfile.State(0,velocity), getNextState(path)).velocity;
+          getNextState(path);
           double xSpeed=dX/total*velocity;
           double ySpeed=dY/total*velocity;
           drive.driveSpeed(xSpeed, ySpeed, 0, true, false);
     }
+    private TrapezoidProfile.State getNextState(Trajectory path){
+        for(State state:path.getStates()){
+            if(state.curvatureRadPerMeter<1E-4)continue;
+            double maxAllowedVelocity=Math.PI/2/Math.abs(state.curvatureRadPerMeter)/Math.sqrt(1+Math.pow(maxAcceleration,-2));
+            if(maxAllowedVelocity<maxVelocity){
+                return new TrapezoidProfile.State(state.timeSeconds,maxAllowedVelocity);
+            }
+        }
+        return new TrapezoidProfile.State(path.getTotalTimeSeconds()-(path.getStates().get(path.getStates().size()-1).timeSeconds-path.getStates().get(path.getStates().size()-2).timeSeconds), 0);
+    }
     public boolean isFinished(){
-        return done;
+        return false;
     }
 }
