cd0ef3d9a322aba9f73708fe31f137cea0a8fa06
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2024-04-05 22:13:51 -0500

    Javadocs, 2024 field, remove continuous

--- src/main/java/frc/robot/commands/PathingCommand.java
+++ src/main/java/frc/robot/commands/PathingCommand.java
@@ -20,38 +20,76 @@ import me.nabdev.pathfinding.structures.ImpossiblePathException;
 import me.nabdev.pathfinding.structures.Path;
 import me.nabdev.pathfinding.utilities.FieldLoader.Field;
 
+/** A command to go to the given position. */
 public class PathingCommand extends Command {
+  /**
+   * Statically configured robot profile that will be used by default when a new PathingCommand is
+   * created.
+   */
   private static RobotProfile defaultRobotProfile;
+
+  /** The robot profile for this instance of the PathingCommand. */
   private RobotProfile robotProfile;
+
+  /** Supplies the robot's current position. */
   private static Supplier<Pose2d> robotPose;
+
+  /** Consumer to drive the robot given ChassisSpeeds. */
   private static Consumer<ChassisSpeeds> drive;
+
+  /** The Pathfinder that is used for generating the paths. */
   private static Pathfinder pathfinder;
+
   private double velocity, rotationalVelocity = 0;
   private TrapezoidProfile translationProfile, rotationProfile;
+
+  /** The goal position to end at. */
   private Pose2d goalPose;
+
+  /**
+   * A field that shows the next goal pose to be reached in the process of getting to the final one.
+   */
   private Field2d nextPoseFieldDisplay = new Field2d();
+
+  /** A field that shows the final goal pose. */
   private Field2d finalPoseFieldDisplay = new Field2d();
-  private boolean continnuous = false;
+
+  /** The default amount this can be off by when considered to be complete. */
   private static double defaultTranslationTolerance = .05, defaultRotationTolerance = Math.PI / 32;
+
+  /**
+   * The amount this can be off by for this instance of PathingCommand when considered to be
+   * complete.
+   */
   private double translationTolerance = defaultTranslationTolerance,
       rotationTolerance = defaultTranslationTolerance;
+
+  /** The drive subsystem. */
   private static Subsystem subsystem;
 
   private static final double dT = .02, eps = 1E-4;
 
+  /**
+   * Constructs a PathingCommand to go to the given position.
+   *
+   * @param pose The goal position.
+   * @throws NullPointerException If the robot profile, pose supplier, drive speed consumer, or
+   *     drive subsystem is null. Please call {@link PathingCommand#setRobot} and {@link
+   *     PathingCommand#setDefaultRobotProfile} before constructing a PathingCommand.
+   */
   public PathingCommand(Pose2d pose) {
     if (defaultRobotProfile == null)
       throw new NullPointerException(
           "Default Robot Profile is null, please call PathingCommand.setDefaultRobotProfile before this constructor");
     if (robotPose == null)
       throw new NullPointerException(
-          "Robot Pose supplier is null. Please call PathingCommand.set Robot before this constructor");
+          "Robot Pose supplier is null. Please call PathingCommand.setRobot before this constructor");
     if (drive == null)
       throw new NullPointerException(
-          "Drive Speed consumer is null. Please call PathingCommand.set Robot before this constructor");
+          "Drive Speed consumer is null. Please call PathingCommand.setRobot before this constructor");
     if (subsystem == null)
       throw new NullPointerException(
-          "Drive Subsystem is null. Please call PathingCommand.set Robot before this constructor");
+          "Drive Subsystem is null. Please call PathingCommand.setRobot before this constructor");
     setTolerances(defaultTranslationTolerance, defaultRotationTolerance);
     this.goalPose = pose;
     this.robotProfile = defaultRobotProfile;
@@ -61,10 +99,26 @@ public class PathingCommand extends Command {
     SmartDashboard.putData("Final Pose", finalPoseFieldDisplay);
   }
 
+  /**
+   * Constructs a pathing command to go to the given position.
+   *
+   * @param x The goal x position.
+   * @param y The goal y position.
+   * @param rot The goal rotation.
+   * @throws NullPointerException If the robot profile, pose supplier, drive speed consumer, or
+   *     drive subsystem is null. Please call {@link PathingCommand#setRobot} and {@link
+   *     PathingCommand#setDefaultRobotProfile} before constructing a PathingCommand.
+   */
   public PathingCommand(double x, double y, double rot) {
     this(new Pose2d(x, y, new Rotation2d(rot)));
   }
 
+  /**
+   * Sets a different {@link RobotProfile} for this command than the configured default. To set the
+   * default robot profile, use {@link PathingCommand#setDefaultRobotProfile}
+   *
+   * @param profile The robot profile to set.
+   */
   public PathingCommand setRobotProfile(RobotProfile profile) {
     this.robotProfile = profile;
     translationProfile =
@@ -77,6 +131,15 @@ public class PathingCommand extends Command {
     return this;
   }
 
+  /**
+   * Configures the robot to be able to be referenced by this command.
+   *
+   * @param robotPose Supplier of robot pose. This should usually be a reference to a getPose()
+   *     method.
+   * @param drive Consumer to drive the robot. Must take ChassisSpeeds and be field relative. This
+   *     should usually be a reference to a drive() method.
+   * @param subsystem The drive subsystem (so it can be required).
+   */
   public static void setRobot(
       Supplier<Pose2d> robotPose, Consumer<ChassisSpeeds> drive, Subsystem subsystem) {
     PathingCommand.drive = drive;
@@ -84,19 +147,34 @@ public class PathingCommand extends Command {
     PathingCommand.subsystem = subsystem;
   }
 
+  /**
+   * Sets the default {@link RobotProfile} to be used when a new PathingCommand is constructed.
+   *
+   * @param profile The robot profile to set as the default.
+   */
   public static void setDefaultRobotProfile(RobotProfile robotProfile) {
     PathingCommand.defaultRobotProfile = robotProfile;
     pathfinder =
-        new PathfinderBuilder(Field.CHARGED_UP_2023)
+        new PathfinderBuilder(Field.CRESCENDO_2024)
             .setRobotLength(robotProfile.getLength())
             .setRobotWidth(robotProfile.getWidth())
             .build();
   }
 
+  /**
+   * @return The default {@link RobotProfile} that has been configured.
+   */
   public static RobotProfile getDefaultRobotProfile() {
     return defaultRobotProfile;
   }
 
+  /**
+   * Sets the field/obstacle layout to a custom one from a json. The layout defaults to the 2024
+   * field.
+   *
+   * @param name The name the custom field json file, which must be located in the deploy folder.
+   *     NOT the full path.
+   */
   public static void setCustomField(String name) {
     pathfinder =
         new PathfinderBuilder(Filesystem.getDeployDirectory() + "\\" + name)
@@ -105,6 +183,12 @@ public class PathingCommand extends Command {
             .build();
   }
 
+  /**
+   * Sets the field/obstacle layout to one from the {@link Field} enum. The layout defaults to the
+   * 2024 field.
+   *
+   * @param field The enum value of the desired field.
+   */
   public static void setCustomField(Field field) {
     pathfinder =
         new PathfinderBuilder(field)
@@ -210,25 +294,44 @@ public class PathingCommand extends Command {
     return Math.PI - Math.acos((d1 * d1 + d2 * d2 - d3 * d3) / (2 * d1 * d2));
   }
 
-  public PathingCommand setContinnuous(boolean continnuous) {
-    this.continnuous = continnuous;
-    return this;
-  }
-
+  /**
+   * Sets the tolerances to something different than the default. Should be used if this particular
+   * PathingCommand should have different tolerances than the others. The tolerances are the maximum
+   * allowed error for which the robot is considered to have reached the goal and should be tuned to
+   * your robot. They should be as small as possible without being more precise than the robot can
+   * achieve well. If the tolerance is too small, the robot will spend longer than it should trying
+   * to get perfectly in position (and move the wheels in different directions as it tries to
+   * perfectly adjust). If it is at a good amount, it should stop as soon as it reaches the position
+   * (the wheels moving back and forth should not be noticeable).
+   *
+   * @param translationTolerance The translation tolerance to set. In meters. Defaults to 5 cm.
+   * @param rotationTolerance The rotation tolerance to set. In radians. Defaults to pi/32.
+   */
   public PathingCommand setTolerances(double translationTolerance, double rotationTolerance) {
     this.translationTolerance = translationTolerance;
     this.rotationTolerance = rotationTolerance;
     return this;
   }
 
+  /**
+   * Sets the default tolerances to be used when a new pathing command is constructed. The
+   * tolerances are the maximum allowed error for which the robot is considered to have reached the
+   * goal and should be tuned to your robot. They should be as small as possible without being more
+   * precise than the robot can achieve well. If the tolerance is too small, the robot will spend
+   * longer than it should trying to get perfectly in position (and move the wheels in different
+   * directions as it tries to perfectly adjust). If it is at a good amount, it should stop as soon
+   * as it reaches the position (the wheels moving back and forth should not be noticeable).
+   *
+   * @param translationTolerance The translation tolerance to set. In meters. Defaults to 5 cm.
+   * @param rotationTolerance The rotation tolerance to set. In radians. Defaults to pi/32.
+   */
   public static void setDefaultTolerances(double translationTolerance, double rotationTolerance) {
     PathingCommand.defaultTranslationTolerance = translationTolerance;
     PathingCommand.defaultRotationTolerance = rotationTolerance;
   }
 
   public boolean isFinished() {
-    return !continnuous
-        && (robotPose.get().getTranslation().getDistance(goalPose.getTranslation())
+    return (robotPose.get().getTranslation().getDistance(goalPose.getTranslation())
                     + velocity * velocity / 2 / robotProfile.getMaxAcceleration()
                 < translationTolerance
             && Math.abs(robotPose.get().getRotation().minus(goalPose.getRotation()).getRadians())
