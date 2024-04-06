d7ef55628bfe334edf3daf9225bce2ad5f6c8541
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2024-04-05 22:14:34 -0500

    Javadocs, gear inner class

--- src/main/java/frc/robot/robotprofile/Motor.java
+++ src/main/java/frc/robot/robotprofile/Motor.java
@@ -3,6 +3,17 @@ package frc.robot.robotprofile;
 public class Motor {
   private double stallTorque, freeSpeed;
 
+  /**
+   * Constructs a custom motor. In most cases, you should be able to just use {@link Motor#CIM()},
+   * {@link Motor#NEO()}, {@link Motor#FALCON()}, {@link Motor#VORTEX()}, or {@link
+   * Motor#KRAKENX60()}, and then use the {@link #gear()} method to get it in the correct gear. If
+   * you want to make a custom motor, {@link https://www.reca.lc/} should be used to find the
+   * values.
+   *
+   * @param maxStallTorque The maximum stall torque.
+   * @param realStallTorque The real stall torque.
+   * @param freeSpeed The free speed.
+   */
   public Motor(double maxStallTorque, double realStallTorque, double freeSpeed) {
     this.stallTorque = realStallTorque;
     this.freeSpeed = freeSpeed;
@@ -24,39 +35,49 @@ public class Motor {
     this.freeSpeed = freeSpeed;
   }
 
+  /**
+   * Gears the motor.
+   *
+   * @param gearRatio The gear ratio can be a custom one (given as a double), but in most cases, a
+   *     value in {@link Gear} should be able to be used.
+   * @return A motor with the same settings as the current one but with the given gear.
+   */
   public Motor gear(double gearRatio) {
     freeSpeed /= gearRatio;
     stallTorque *= gearRatio;
     return this;
   }
 
-  public static final double SDS_L1 = 8.14,
-      SDS_L2 = 6.75,
-      SDS_L3 = 6.12,
-      SDS_L4 = 5.14,
-      REV_LOW = 5.5,
-      REV_MED = 5.08,
-      REV_HIGH = 4.71,
-      REV_EHIGH_1 = 4.5,
-      REV_EHIGH_2 = 4.29,
-      REV_EHIGH_3 = 4.00,
-      REV_EHIGH_4 = 3.75,
-      REV_EHIGH_5 = 3.56,
-      WCP_X1_LOW = 7.85,
-      WCP_X1_MED = 7.13,
-      WCP_X1_HIGH = 6.54,
-      WCP_X2_LOW = 6.56,
-      WCP_X2_MED = 5.96,
-      WCP_X2_HIGH = 5.46,
-      WCP_X3_LOW = 5.14,
-      WCP_X3_MED = 4.75,
-      WCP_X3_HIGH = 4.41,
-      WCP_XS1_12 = 6.00,
-      WCP_XS1_13 = 5.54,
-      WCP_XS1_14 = 5.14,
-      WCP_XS2_14 = 4.71,
-      WCP_XS2_15 = 4.40,
-      WCP_XS2_16 = 4.13;
+  /** Contains most gear ratios that would be used. */
+  public static final class Gear {
+    public static final double SDS_L1 = 8.14,
+        SDS_L2 = 6.75,
+        SDS_L3 = 6.12,
+        SDS_L4 = 5.14,
+        REV_LOW = 5.5,
+        REV_MED = 5.08,
+        REV_HIGH = 4.71,
+        REV_EHIGH_1 = 4.5,
+        REV_EHIGH_2 = 4.29,
+        REV_EHIGH_3 = 4.00,
+        REV_EHIGH_4 = 3.75,
+        REV_EHIGH_5 = 3.56,
+        WCP_X1_LOW = 7.85,
+        WCP_X1_MED = 7.13,
+        WCP_X1_HIGH = 6.54,
+        WCP_X2_LOW = 6.56,
+        WCP_X2_MED = 5.96,
+        WCP_X2_HIGH = 5.46,
+        WCP_X3_LOW = 5.14,
+        WCP_X3_MED = 4.75,
+        WCP_X3_HIGH = 4.41,
+        WCP_XS1_12 = 6.00,
+        WCP_XS1_13 = 5.54,
+        WCP_XS1_14 = 5.14,
+        WCP_XS2_14 = 4.71,
+        WCP_XS2_15 = 4.40,
+        WCP_XS2_16 = 4.13;
+  }
 
   public static Motor CIM() {
     return new Motor(2.4, .686, 5333);
