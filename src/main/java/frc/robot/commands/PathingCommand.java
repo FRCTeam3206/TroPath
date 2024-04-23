66a4a4d4fa095a4ed790fa8870d1eb018d3bf9c7
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2024-04-23 08:32:55 -0500

    Add ability to execute commands at distances from the goal along the path.

--- src/main/java/frc/robot/commands/PathingCommand.java
+++ src/main/java/frc/robot/commands/PathingCommand.java
@@ -9,7 +9,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.robotprofile.RobotProfile;
+import frc.utils.CommandDuringPath;
+
+import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import me.nabdev.pathfinding.Pathfinder;
 import me.nabdev.pathfinding.structures.ImpossiblePathException;
@@ -30,6 +34,8 @@ public class PathingCommand extends Command {
   private static final double dT = .02;
   private PathProfiler pathProfiler;
   private boolean linearPhysics = false;
+  private ArrayList<CommandDuringPath> inactiveCommands = new ArrayList<CommandDuringPath>();
+  private ArrayList<CommandDuringPath> activeCommands = new ArrayList<CommandDuringPath>();
 
   /**
    * Constructs a PathingCommand. This method is called by the {@link PathingCommandGenerator}.
@@ -64,6 +70,57 @@ public class PathingCommand extends Command {
                 profile.getMaxRotationalVelocity(), profile.getMaxRotationalAcceleration()));
     pathProfiler = new PathProfiler(profile.getMaxVelocity(), profile.getMaxAcceleration());
     return this;
+  }
+
+  public void addCommandWithEndDist(Command command, double dist) {
+    inactiveCommands.add(new CommandDuringPath(command, -1, dist));
+  }
+
+  public void addCommandWithStartDist(Command command, double startDist) {
+    inactiveCommands.add(new CommandDuringPath(command, startDist, -1));
+  }
+
+  public void addCommandWithStartEndDist(Command command, double startDist, double endDist) {
+    inactiveCommands.add(new CommandDuringPath(command, startDist, endDist)); // The constructor will hand mixed up start and end by assuming intention of correct order.
+  }
+
+  public void addCommandDistBasedCondition (Command command, Predicate<Double> isActive) {
+    inactiveCommands.add(new CommandDuringPath(command, isActive));
+  }
+
+  private void checkInactiveCommands(double dist) {
+    for (int i = 0; i < inactiveCommands.size();) {
+      CommandDuringPath currentCommand = inactiveCommands.get(i);
+      if (currentCommand.getIsActive(dist)) {
+        currentCommand.getCommand().initialize();
+        inactiveCommands.remove(i);
+        activeCommands.add(currentCommand);
+      } else {
+        i++;
+      }
+    }
+  }
+
+  private void executeActiveCommands() {
+    for (int i = 0; i < activeCommands.size(); i++) {
+      activeCommands.get(i).getCommand().execute();
+    }
+  }
+
+  private void checkActiveCommands(double dist) {
+    for (int i = 0; i < activeCommands.size();) {
+      CommandDuringPath currentCommand = activeCommands.get(i);
+      if (!currentCommand.getIsActive(dist)) {
+        currentCommand.getCommand().end(true);
+        activeCommands.remove(i);
+        inactiveCommands.add(currentCommand);
+      } else if (currentCommand.getCommand().isFinished()) {
+        currentCommand.getCommand().end(false);
+        activeCommands.remove(i);
+      } else {
+        i++;
+      }
+    }
   }
 
   public void execute() {
@@ -102,6 +159,9 @@ public class PathingCommand extends Command {
     } else {
       velocity = pathProfiler.getNextRobotSpeed(velocity, path.asPose2dList());
     }
+    checkInactiveCommands(pathProfiler.getDistanceToGoal());
+    executeActiveCommands();
+    checkActiveCommands(pathProfiler.getDistanceToGoal());
     SmartDashboard.putNumber("Distance To Goal",pathProfiler.getDistanceToGoal());
     SmartDashboard.putNumber("Physics Time", System.currentTimeMillis() - start);
     SmartDashboard.putNumber("Velocity", velocity);
